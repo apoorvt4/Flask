f6c0bb15686d6bf261cd57886f9a0d9a11f8fb96
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-04 21:03:30 +0530

    server

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-msg="Welcome to my channel"
-print(msg)
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return'Welcome To My youtube channel'
+
+if __name__=='__main__':
+    app.run()
